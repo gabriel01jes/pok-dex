e6a677ec5e24ed434d3c4f9c725077a9fcf42db6
gabriel01jes <gabriel.jesuz.santos@escola.pr.gov.br>
2025-10-16 12:15:59 -0300

    upp

---
+++ app.js
@@ -0,0 +1,9 @@
+const pokemon_numbler = document.getElementById("pkum")
+const pokemon_name = document.getElementById("pkname")
+const pokemon_img = document.getElementById("pkimg")
+
+const pokemon_form = document.getElementById("form")
+const pokemon_input = document.getElementById("imput")
+
+const pokemon_prev = document.getElementById("bnt-prev")
+const pokemon_next = document.getElementsByTagName("bnt-next")
