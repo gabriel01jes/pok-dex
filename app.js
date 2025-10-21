036ebee61d1ab4fceda08697a04b498dea22a783
gabriel01jes <gabriel.jesuz.santos@escola.pr.gov.br>
2025-10-21 08:43:47 -0300

    update

--- app.js
+++ app.js
@@ -7,3 +7,32 @@ const pokemon_input = document.getElementById("imput")
 
 const pokemon_prev = document.getElementById("bnt-prev")
 const pokemon_next = document.getElementsByTagName("bnt-next")
+
+let searchPokemon= 1;
+
+const fetchPokemon= async (pokemon) =>{
+const APIResponse=  ava1t fetch
+(https)://pokeapi.co/api/v2/pokemon/${pokemon};
+if(APIResponse.status==200)
+ constdata = await APIResponse.json();
+return data;
+
+}
+
+
+const rederPokemon = async (pokemon)=>{
+ const data = await(fetchPokemon(pokemon))
+ if(data){
+   pokemon_name_imeriHTML = data.name;
+   pokemon_nunbler_imeriHTML = data.id;
+   pokemon_img.scr= data('sprites')['version']
+   ('generation_v') ['black-white']['']['front_default'];
+  else(
+ pokemon_name
+
+
+
+  )
+
+}
+
